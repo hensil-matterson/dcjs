492b314d80e8ba2c917a4b81304286c9669f148b
hensil-matterson <ygorrhenr@gmail.com>
2026-05-07 13:57:43 -0300

    Update menu.js

--- script/menu.js
+++ script/menu.js
@@ -13,7 +13,7 @@ const fundos = {
 let imperios = [];
 let imperioSelecionado = null;
 
-fetch("script/imperios.json")
+fetch("script/desc_imperios.json")
     .then(res => res.json())
     .then(data => {
         imperios = data;
